60b6a9c046d7a0af451e985d85ed276d6707e6fb
MoahmedALNaser <mo7ammednaser092@gmail.com>
2023-03-28 19:48:09 +0200

    update files

--- js/main.js
+++ js/main.js
@@ -9,14 +9,23 @@ let pText = [
   "لماذا نحن الأفضل ؟ نحن الأفضل لأننا نقدم أفضل خدمة وبأقل الأسعار وخدمة متواصلة طوال اليوم ",
   "خدمة تفتخر بها",
 ];
+let images = ["1.webp", "2.jpeg", "3.webp"];
+let bubble = document.querySelectorAll(" .transiant span");
 // change the background image of the home section every 5 seconds
 let i = 2;
 
 setInterval(() => {
   console.log(i - 1);
-  home.style.backgroundImage = `url('./img/img ${i}.jpeg')`;
+  home.style.backgroundImage = `url('./img/img ${images[i - 1]}')`;
   homeH1.textContent = h1Text[i - 1];
   homep.textContent = pText[i - 1];
+  bubble[i - 1].classList.add("active");
+  bubble.forEach((item, index) => {
+    if (index != i - 1) {
+      item.classList.remove("active");
+    }
+  });
+
   i++;
   if (i > 3) {
     i = 1;
